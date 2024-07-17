dbb54cff2e56a2d9f0ce26148437a2cd1e45fb97
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-17 10:00:11 +0530

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,11 @@
+public class ATM{
+  static double Balance=30000;
+  public double Balance(double deposit,double Withdrawl){
+  // Write a Condition that satisfy logic of ATM Deposit and Withdrawl
+  return Balance;
+  }
+  public static void main(String[]args){
+  ATM a=new ATM();
+    // Deposit and WithDrawl Amount should be taken in scanner;
+    // Print the Balance here
+  }}
