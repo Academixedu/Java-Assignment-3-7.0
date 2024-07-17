8c70017ba1342a94765dcd7865fd479ccde1431f
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-07-17 13:24:37 +0530

    first commit

--- Statics.java
+++ Statics.java
@@ -1,8 +1,13 @@
 public class Statics{
 // Create a Static method with return type String
+  public static String Name(){
+    return "Karthik";
+  }
  
   public static void main(String[]args){
     //Call the Method and Print the value of return type in output
+    String name=Name();
+    System.out.println(name); 
     
   }
 }
--- WhileLoop.java
+++ WhileLoop.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class WhileLoop{
   public static void main(String []args){
 // Take Your Name in Scanner 
     // Using While Loop Print it for 5 times 
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter your Name: ");
+    String name=sc.nextLine();
+    int i=0;
+    while(i<5){
+      System.out.println(name+" "+i);
+      i++;
+    }
   }
+}
