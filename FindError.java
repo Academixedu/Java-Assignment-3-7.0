65fc06a812d376a806378e17644ae1f4453a45d3
ganeshteki <ganeshteki2001@gmail.com>
2024-07-17 13:24:29 +0530

    fe

--- FindError.java
+++ FindError.java
@@ -1,11 +1,11 @@
 public class FindError{
-public int void m1(int a,double b){
-a=a*b;
-  return a;
+public double m1(int a,double b)
+{
+  return a*b;
 }
   // Method must contain return 
 public static void main(String[]args){
-FindError f=FindError();
-  f.m1();
+FindError f=new FindError();
+ System.out.println("product="+f.m1(7,8));
 }
 }
