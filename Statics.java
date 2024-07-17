22e3eb0a576a99b4401f2368592f6e40037fc905
nayeem6836 <maseednayeem@gmail.com>
2024-07-17 17:16:29 +0530

    Update Statics.java

--- Statics.java
+++ Statics.java
@@ -1,11 +1,13 @@
 public class Statics{
 // Create a Static method with return type String
- 
+ public Static String name(String n)
+ {
+  return n;
+ }
   public static void main(String[]args){
     //Call the Method and Print the value of return type in output
-    
-  }
-  Public static String name(name ){
-    return name;
+   Static s new Statics();
+   String resutls= s.name("nayeem");
+   System.out.println("my name is "+result);
   }
 }
