0e7ce3198764b599896f3a378f328e657bd447d4
goutham120203 <gouthampaladugu12@gmail.com>
2024-07-17 13:38:45 +0530

    goutham

--- Statics.java
+++ Statics.java
@@ -1,8 +1,14 @@
 public class Statics{
 // Create a Static method with return type String
+public static String str(String name){
+  return name;
+}
  
   public static void main(String[]args){
     //Call the Method and Print the value of return type in output
+    Statics s=new Statics();
+    String sc=s.str("goutham");
+    System.out.println("name is "+sc);
     
   }
 }
