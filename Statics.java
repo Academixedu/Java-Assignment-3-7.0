e0acc297e2cb7583c77c829dbe4bbda2297438b1
12Rameshwar <Chavhanrameshwar13@gmail.com>
2024-07-17 15:29:12 +0530

    Update Statics.java

--- Statics.java
+++ Statics.java
@@ -1,8 +1,15 @@
+
 public class Statics{
-// Create a Static method with return type String
- 
-  public static void main(String[]args){
-    //Call the Method and Print the value of return type in output
+    public static void name(String n){
+        System.out.println("ram");
+    }
+    // Create a Static method with return type String
     
-  }
+      public static void main(String[]args){
+        name("ram");
+
+     
+    }
 }
+
+    
