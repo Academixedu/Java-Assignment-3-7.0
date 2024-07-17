604e36135780ef93a7a6283931d1682c3d48b184
nayeem6836 <nayeemmaseed9100@gmail.com>
2024-07-17 12:28:02 +0530

    commit

--- ForLoop.java
+++ ForLoop.java
@@ -4,8 +4,8 @@ public class ForLoop{
 // Find out the Error in for loop and correct it
 // assign the error to variable s and print out side for loop
  String s="";   
-for(int i=10;i>=0;i++){
-System.out.println("Hello World !!!!");
+for(int i=10;i>0;i--){
+System.out.println("Hello World !!!!"+i);
 }
     System.out.println(s);
 }
