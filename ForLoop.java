e63b51eebe75c92f8995e690fca951956a7c2280
Astikborkar <astikbokar3481@gmail.comr>
2024-07-17 14:14:59 +0530

    Astik

--- ForLoop.java
+++ ForLoop.java
@@ -3,9 +3,9 @@ public class ForLoop{
   public static void main(String[]args){
 // Find out the Error in for loop and correct it
 // assign the error to variable s and print out side for loop
- String s="";   
-for(int i=10;i>=0;i++){
-System.out.println("Hello World !!!!");
+ String s=" change i ++ to i-- ";   
+for(int i=10;i>=0;i--){
+System.out.println("Hello World !!!!"+i);
 }
     System.out.println(s);
 }
--- Statics.java
+++ Statics.java
@@ -1,8 +1,15 @@
 public class Statics{
+private static final String String = null;
+
 // Create a Static method with return type String
+public static String main() {
+return "astik";
+}
  
   public static void main(String[]args){
     //Call the Method and Print the value of return type in output
-    
+    System.out.println("my name is "+ main());
+  
+
   }
 }
