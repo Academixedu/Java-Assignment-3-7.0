5c25783fb527d62cc0fca471dd147a5148589a74
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-17 14:08:26 +0530

    Update Statics.java

--- Statics.java
+++ Statics.java
@@ -1,8 +1,11 @@
 public class Statics{
 // Create a Static method with return type String
- 
+public static String myStaticMethod() {
+  return ("Hello World!");
+ }
   public static void main(String[]args){
     //Call the Method and Print the value of return type in output
-    
+    String result = myStaticMethod();
+        System.out.println(result);
   }
 }
