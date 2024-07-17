4db32e2cd81887126816eb55dd135704939e85a0
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-17 10:35:54 +0530

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,8 @@
+public class WhileLoop{
+  public static void main(String []args){
+  int h=10;
+    // Write a while loop iterations for 5 times such that h should be multiplied with 5 on eacg iteration and re assign value, again it should be multiply with 5.
+    // 10*5=50 print 50
+    // 50*5=250 print 250
+    // Like Wise Change the value on 5 iterations and print the changed value on each iteration
+  }
