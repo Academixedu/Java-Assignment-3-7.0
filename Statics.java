4f93a63d1476d6588d4e35e44ae4eb01cd603adb
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-17 11:29:40 +0530

    Create Statics.java

---
+++ Statics.java
@@ -0,0 +1,8 @@
+public class Statics{
+// Create a Static method with return type String
+ 
+  public static void main(String[]args){
+    //Call the Method and Print the value of return type in output
+    
+  }
+}
