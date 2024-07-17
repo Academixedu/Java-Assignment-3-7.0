1b84fd3dc117012de9e57e4833aa60dec97f1969
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-17 14:38:06 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,13 +1,15 @@
 public class ATM{
   double Balance=30000;
   public double Balance(double deposit,double Withdrawl){
-  Balance=Balance+deposit
-    if(Balance<=withdrawl){
-    Balance=Balance-withdrawl;
-  System.out.println("Balance is in "+' + '+ " value");
+  Balance=Balance+deposit;
+  
+    if(Balance<=Withdrawl){
+      Balance=Balance-Withdrawl;
+  System.out.println("Balance is in - value");
   }
-    else{
-    System.out.println("Balance in"+" - "+" Value");
+    else
+    {
+    System.out.println("Balance in + Value");
     }
   return Balance;
   }
@@ -15,3 +17,5 @@ public class ATM{
   ATM a=new ATM();
     a.Balance(3400,45000);
   }}
+
+
