ecd2bf66fffc6138ff76ef697447450490144a4c
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-17 14:40:51 +0530

    Update Statics.java

--- Statics.java
+++ Statics.java
@@ -1,7 +1,12 @@
 public class Statics{
+  public static void name(String n){
+    System.out.println("sarvsh");
+  }
 // Create a Static method with return type String
  
   public static void main(String[]args){
+    name("sarvesh");
+
     //Call the Method and Print the value of return type in output
     
   }
