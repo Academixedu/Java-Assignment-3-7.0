24ea39d649a943b782a28577b889fc022de6f719
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-17 12:54:32 +0530

    assinment3

--- FindError.java
+++ FindError.java
@@ -1,11 +1,11 @@
 public class FindError{
-public int void m1(int a,double b){
-a=a*b;
-  return a;
+public void m1(int a,int b){
+ a=a*b;
+ System.out.println(a);
 }
   // Method must contain return 
 public static void main(String[]args){
-FindError f=FindError();
-  f.m1();
+FindError f=new FindError();
+  f.m1(2,3);
 }
 }
--- WhileLoop.java
+++ WhileLoop.java
@@ -1,5 +1,18 @@
+
+import java.util.Scanner;
+
 public class WhileLoop{
   public static void main(String []args){
 // Take Your Name in Scanner 
+  Scanner sc = new Scanner(System.in);
+  System.out.println(" enter name");
+  String name=sc.nextLine();
+    int count=0;
+    while(count<5){
+    System.out.println(" swarupa");
     // Using While Loop Print it for 5 times 
+    count++;
+    }
   }
+
+}
