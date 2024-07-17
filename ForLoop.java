ebaba89993e85c9da85865fba67b1b60ce188214
2110Abhishek <agchoudhari2110@gmail.com>
2024-07-17 14:08:01 +0530

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -3,10 +3,10 @@ public class ForLoop{
   public static void main(String[]args){
 // Find out the Error in for loop and correct it
 // assign the error to variable s and print out side for loop
- String s="";   
-for(int i=10;i>=0;i++){
-System.out.println("Hello World !!!!");
+  
+for(int i=0;i<=10;i++){
+System.out.println("Hello World !!!!"+i);
 }
-    System.out.println(s);
+  
 }
 }
