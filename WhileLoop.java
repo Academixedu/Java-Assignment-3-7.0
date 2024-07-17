d97e8a139dfc1e312be7d2afff04aee42a31a1b4
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-17 12:34:43 +0530

    mani

--- WhileLoop.java
+++ WhileLoop.java
@@ -5,13 +5,13 @@ public class WhileLoop{
   public static void main(String []args)
   {
 // Take Your Name in Scanner 
-    // Using While Loop Print it for 5 times 
+    // Using While Lop Print it for 5 times 
     Scanner scan = new Scanner(System.in);
     
     System.out.println("Enter your name");
     String name = scan.nextLine();
     int count=0;
-    while(count <=5)
+    while(count <5)
     {
       System.out.println("name is "+name);
       count ++;
