123ad079fdc3f4cebe0afc8999e05a914d66abd4
nayeem6836 <maseednayeem@gmail.com>
2024-07-17 17:07:56 +0530

    Update FindError.java

--- FindError.java
+++ FindError.java
@@ -1,10 +1,16 @@
 public class FindError{
 public static void m1(int a,int b){
 a=a*b;
+  public int m1(int a,int b){
+    a=a*b;
+    return a;
 }
   // Method must contain return 
 public static void main(String[]args){
 FindError f= new FindError();
-  f.m1(2,3);
+  f.m1();
+  FindError f = new FindError();
+  int result = f.m1(4,7);
+  System.out.println("product is "+result);
 }
 }
