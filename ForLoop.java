93857c745d440d049bddecf6c085a466d720881f
Verat-hub <venkey482thalka@gmail.com>
2024-07-20 18:50:59 +0530

    Venkat

--- ForLoop.java
+++ ForLoop.java
@@ -1,12 +1,9 @@
 public class ForLoop{
-
   public static void main(String[]args){
-// Find out the Error in for loop and correct it
-// assign the error to variable s and print out side for loop
- String s="";   
-for(int i=10;i>=0;i++){
-System.out.println("Hello World !!!!");
+ String s="i=0,i<=10";   
+for(int i=0;i<=10;i++){
+System.out.println("Hello World !!!!"+i);
 }
-    System.out.println(s);
+System.out.println(s);
 }
 }
--- Statics.java
+++ Statics.java
@@ -1,8 +1,13 @@
 public class Statics{
-// Create a Static method with return type String
- 
-  public static void main(String[]args){
-    //Call the Method and Print the value of return type in output
-    
+  // Create a Static method with return type String
+   public static String ill(String li){
+   return li; 
+   }
+    public static void main(String[]args){
+      Statics kill=new Statics();
+      kill.ill("Vers");
+      System.out.println(ill("Vers"));//Call the Method and Print the value of return type in output
+      
+    }
   }
-}
+  
